b96ce811b973a72272b5ccdef20b5024d114f056
prakash <prakash.kondamaraju@gmail.com>
2025-03-11 07:50:39 +0530

    -m "Math class example"

---
+++ src/test/java/com/programmingpractise/MathExample.java
@@ -0,0 +1,15 @@
+package com.programmingpractise;
+
+public class MathExample {
+    public static void main(String[] args) {
+    double result;
+    result=Math.PI;
+    result=Math.max(25,45);
+    result=Math.floor(3.99);
+    result=Math.ceil(3.1);
+    result=Math.sqrt(6);
+    result=Math.abs(-9);
+    result=Math.pow(2,6);
+        System.out.println(result);
+    }
+}
